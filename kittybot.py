b47ac5dc18366f10c67d0a349ec9698725376674
Дмитрий Здор <zdors@yandex.ru>
2025-10-01 20:02:10 +0700

    капибары и лисы

--- kittybot.py
+++ kittybot.py
@@ -9,60 +9,53 @@ load_dotenv()
 
 secret_token = os.getenv('TOKEN')
 
+URL_DOG = 'https://api.thecdogapi.com/v1/images/search'
 
-URL_CAT = 'https://api.thecatapi.com/v1/images/search'
-URL_DOG = 'https://api.thedogapi.com/v1/images/search'
+DATA = {
+    '/cat': ('https://api.thecatapi.com/v1/images/search', 0),
+    '/fox': ('https://randomfox.ca/floof/', 1),
+    '/dog': ('https://random.dog/woof.json', 2),
+    '/capy': ('https://api.capy.lol/v1/capybara?json=true', 3),
+}
 
+menu = list(DATA.keys())
 
-def get_new_image_cat():
+def get_new_image(animal):
     try:
-        response = requests.get(URL_CAT)
+        response = requests.get(DATA[animal][0])
     except Exception as error:
         # print(error)
         logging.error(f'Ошибка при запросе к основному API: {error}')
         response = requests.get(URL_DOG)
+        response = response.json()
+        return response[0].get('url')
 
     response = response.json()
-    random_cat = response[0].get('url')
-    return random_cat
-
-def get_new_image_dog():
-    try:
-        response = requests.get(URL_DOG)
-    except Exception as error:
-        # print(error)
-        logging.error(f'Ошибка при запросе к основному API: {error}')
-        response = requests.get(URL_CAT)
-
-    response = response.json()
-    random_dog = response[0].get('url')
-    return random_dog
+    print(response)
+    idx = DATA[animal][1]
+    if idx == 0:
+        return response[0].get('url')
+    elif idx == 1:
+        return response["link"]
+    elif idx == 2:
+        return response["url"]
+    elif idx == 3:
+        return response["data"]["url"]
 
 
-def new_cat(update, context):
+def new(update, context):
+    animal = update.message.text
+    print(animal)
     chat = update.effective_chat
     name = update.message.chat.first_name
-    button = ReplyKeyboardMarkup([['/newcat', '/newdog']], resize_keyboard=True)
+    button = ReplyKeyboardMarkup([DATA], resize_keyboard=True)
     context.bot.send_message(
         chat_id=chat.id,
-        text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+        text=f'Привет, {name}. Посмотри, какого красавчика я тебе нашёл',
         reply_markup=button
     )
 
-    context.bot.send_photo(chat.id, get_new_image_cat())
-
-
-def new_dog(update, context):
-    chat = update.effective_chat
-    name = update.message.chat.first_name
-    button = ReplyKeyboardMarkup([['/newcat', '/newdog']], resize_keyboard=True)
-    context.bot.send_message(
-        chat_id=chat.id,
-        text='Привет, {}. Посмотри, какого песика я тебе нашёл'.format(name),
-        reply_markup=button
-    )
-
-    context.bot.send_photo(chat.id, get_new_image_dog())
+    context.bot.send_photo(chat.id, get_new_image(animal))
 
 
 def wake_up(update, context):
@@ -71,7 +64,7 @@ def wake_up(update, context):
     chat = update.effective_chat
     name = update.message.chat.first_name
 
-    button = ReplyKeyboardMarkup([['/newcat', '/newdog']], resize_keyboard=True)
+    button = ReplyKeyboardMarkup([DATA], resize_keyboard=True)
     context.bot.send_message(
         chat_id=chat.id,
         text=f'Спасибо {name}, что включили меня\n'
@@ -86,8 +79,10 @@ def main():
     # он будет отфильтровывать только сообщения с содержимым '/start'
     # и передавать их в функцию wake_up()
     updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
-    updater.dispatcher.add_handler(CommandHandler('newdog', new_dog))
+    updater.dispatcher.add_handler(CommandHandler('cat', new))
+    updater.dispatcher.add_handler(CommandHandler('capy', new))
+    updater.dispatcher.add_handler(CommandHandler('fox', new))
+    updater.dispatcher.add_handler(CommandHandler('dog', new))
     # Метод start_polling() запускает процесс polling,
     # приложение начнёт отправлять регулярные запросы для получения обновлений.
     updater.start_polling()
